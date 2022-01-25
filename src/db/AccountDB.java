bbaca54fbca1c10f3bd17705c7b5c70f659261ee
Camila Ribeiro de Moraes <72588121+camilarmoraes@users.noreply.github.com>
2022-01-25 19:15:03 -0300

    Update

--- src/db/AccountDB.java
+++ src/db/AccountDB.java
@@ -13,8 +13,51 @@ public class AccountDB {
 
 
 
-	public boolean existeUser(Account user) throws Exception {
+	public boolean existeUser(Account user) throws SQLException {
+		String sql = "select * from account where number = '"+user.getNumber()+"' and account_password = '"+user.getAccountPassword()+"'";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.execute();
+		ResultSet resultSet = statement.getResultSet();
+		return resultSet.next();
 		
+		
+	}
+	public double getBalance(String numberAccount) throws SQLException{
+		
+		String sql = "select balance from account where number = '"+numberAccount+"'";
+		PreparedStatement statement;
+		
+		statement = connection.prepareStatement(sql);
+		//statement.setString(1,numberAccount);
+		statement.execute();
+		
+		ResultSet resultSet = statement.getResultSet();
+		
+		if (resultSet.next()) {
+			return resultSet.getDouble("balance");
+		}
+		return 0;	
+		
+	}
+
+
+
+	public String getnumberAccount(String passwordAccount) throws SQLException {
+
+		String sql = "select number from account where account_password = '"+passwordAccount+"'";
+		PreparedStatement statement;
+		
+		statement = connection.prepareStatement(sql);
+		statement.execute();
+		
+		ResultSet resultSet = statement.getResultSet();
+		
+		if(resultSet.next()) {
+			return resultSet.getString("account_password");
+		}
+		return null;
+		
+			
 		
 	}
 	
